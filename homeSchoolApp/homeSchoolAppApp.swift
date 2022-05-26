9fe1c95389492effa34c1c3de72b9038d17bdfee
Richard Kelly <richard.kelly@goinstore.com>
2022-05-26 20:31:21 +0200

    projectSetup
    - Starting Point

---
+++ homeSchoolApp/homeSchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeSchoolAppApp.swift
+//  homeSchoolApp
+//
+//  Created by Richard Kelly on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct homeSchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
